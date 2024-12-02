c20cee4db0f534502a9a0074f29abd80d96fbcbb
Rizki Zulfihadi <46236372+rizkizulfihadi@users.noreply.github.com>
2024-12-02 10:37:23 +0700

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-
-export const prisma = globalThis.prisma || new PrismaClient();
-
-if(process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+import { PrismaClient } from "@prisma/client"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
